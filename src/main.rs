5679a92865c20d9e3fc8752bbb4812ed949406da
PureFunctor <purefunctor@gmail.com>
2021-05-11 08:32:10 +0800

    Make run a static method, add example file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 mod rustfck;
 
 fn main() {
-    let result = rustfck::interpret("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.
-");
-    if let Err(e) = result {
+    let argv: Vec<String> = std::env::args().into_iter().collect();
+
+    let source = std::fs::read_to_string(&argv[1]);
+
+    if let Ok(s) = source {
+        let interpreter = rustfck::Interpreter::from_source(&s);
+        if let Err(e) = interpreter.and_then(|mut i| i.run()) {
+            println!("panic! {}", e);
+        }
+    } else if let Err(e) = source {
         println!("panic! {}", e);
-    };
+    }
 }
--- src/rustfck.rs
+++ src/rustfck.rs
@@ -85,64 +85,81 @@ impl Instruction {
 type Cell = Wrapping<u8>;
 type Tape = Vec<Cell>;
 
-fn run(instructions: &[Instruction], tape: &mut Tape, pointer: &mut usize) -> Result<(), String> {
-    for instruction in instructions {
-        match instruction {
-            TapeRight => *pointer += 1,
-            TapeLeft => *pointer -= 1,
-            CellInc => {
-                if let Some(c) = tape.get_mut(*pointer) {
-                    *c += Wrapping(1);
+pub struct Interpreter {
+    instructions: Vec<Instruction>,
+    tape: Tape,
+    pointer: usize,
+}
+
+impl Interpreter {
+    pub fn from_source(source: &str) -> Result<Self, String> {
+        Instruction::from_tokens(
+            source
+                .chars()
+                .map(Token::from_char)
+                .flat_map(|t| t.into_iter())
+                .collect(),
+        )
+        .map(|i| Interpreter {
+            instructions: i,
+            tape: vec![Wrapping(0); 30000],
+            pointer: 0,
+        })
+    }
+
+    fn run_(
+        instructions: &[Instruction],
+        tape: &mut Tape,
+        pointer: &mut usize,
+    ) -> Result<(), String> {
+        for instruction in instructions {
+            match instruction {
+                TapeRight => *pointer += 1,
+                TapeLeft => *pointer -= 1,
+                CellInc => {
+                    if let Some(c) = tape.get_mut(*pointer) {
+                        *c += Wrapping(1);
+                    }
                 }
-            }
-            CellDec => {
-                if let Some(c) = tape.get_mut(*pointer) {
-                    *c -= Wrapping(1);
+                CellDec => {
+                    if let Some(c) = tape.get_mut(*pointer) {
+                        *c -= Wrapping(1);
+                    }
                 }
-            }
-            CellPrint => {
-                if let Some(c) = tape.get_mut(*pointer) {
-                    print!("{}", c.0 as char);
+                CellPrint => {
+                    if let Some(c) = tape.get_mut(*pointer) {
+                        print!("{}", c.0 as char);
+                    }
                 }
-            }
-            CellFetch => {
-                let mut i: [u8; 1] = [0; 1];
-                match std::io::stdin().read_exact(&mut i) {
-                    Ok(_) => {
-                        if let Some(c) = tape.get_mut(*pointer) {
-                            *c = Wrapping(i[0]);
+                CellFetch => {
+                    let mut i: [u8; 1] = [0; 1];
+                    match std::io::stdin().read_exact(&mut i) {
+                        Ok(_) => {
+                            if let Some(c) = tape.get_mut(*pointer) {
+                                *c = Wrapping(i[0]);
+                            }
+                        }
+                        Err(_) => {
+                            return Err("cannot fetch".to_string());
                         }
                     }
-                    Err(_) => {
-                        return Err("cannot fetch".to_string());
-                    }
                 }
-            }
-            TapeLoop(i) => {
-                while let Some(n) = tape.get(*pointer) {
-                    if n.0 == 0 {
-                        break;
-                    }
-                    if let Err(e) = run(i, tape, pointer) {
-                        return Err(e);
+                TapeLoop(i) => {
+                    while let Some(n) = tape.get(*pointer) {
+                        if n.0 == 0 {
+                            break;
+                        }
+                        if let Err(e) = Interpreter::run_(i, tape, pointer) {
+                            return Err(e);
+                        }
                     }
                 }
             }
         }
+        Ok(())
     }
-    Ok(())
-}
 
-pub fn interpret(source: &str) -> Result<(), String> {
-    let mut tape: Tape = vec![Wrapping(0); 1024];
-    let mut pointer = 0;
-
-    Instruction::from_tokens(
-        source
-            .chars()
-            .map(Token::from_char)
-            .flat_map(|t| t.into_iter())
-            .collect(),
-    )
-    .and_then(|i| run(&i, &mut tape, &mut pointer))
+    pub fn run(&mut self) -> Result<(), String> {
+        Interpreter::run_(&self.instructions, &mut self.tape, &mut self.pointer)
+    }
 }
